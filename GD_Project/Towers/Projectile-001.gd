c5c0e837effcaa63851a3c5ea4119a3bdf0ab252
Angadeon <angadeon@gmail.com>
2020-08-16 21:31:32 -0700

    created projectile-001

---
+++ GD_Project/Towers/Projectile-001.gd
@@ -0,0 +1,17 @@
+# Projectile-001.gd
+
+extends Area2D
+
+
+const PROJECTILE_SPEED = 100
+
+
+func _ready():
+	pass
+
+
+# This will take the variables and turn it into a movement speed and direction 
+# for the projectile
+func _physics_process(delta):
+	pass
+
