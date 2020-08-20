43be9a0431419fd2d2e87c86324c48a92c7599f1
Isq <mrissq@gmail.com>
2020-08-20 14:30:58 -0700

    Added projectile timeout

--- GD_Project/Towers/Barrel.gd
+++ GD_Project/Towers/Barrel.gd
@@ -10,16 +10,22 @@ var projectileScene = load("res://GD_Project/Towers/Projectiles/Projectile-001.t
 var rotation_dir = 0 # natural state is no rotation
 onready var targetList = get_parent().enemiesInRange # access the enemies list
 
+var elapsed_time = 0
 
 func _physics_process(delta):
-		if targetList:
-			var target = targetList[0] # identify the leading enemy
-			var targetPosition = target.global_position # identify where it is
-			look_at(targetPosition) # look at it. LOOK AT IT!
-			# now the projectiles
-			var projectileInstance = projectileScene.instance()
-			projectileInstance.position = position
-			projectileInstance.rotation_degrees = rotation_degrees
-			projectileInstance.apply_impulse(Vector2(), Vector2(projectileInstance.speed, 0).rotated(rotation))
-			get_parent().add_child(projectileInstance)
+	elapsed_time += delta
+	
+	
+	if targetList and elapsed_time >= 1.1:
+		elapsed_time = 0
+		var target = targetList[0] # identify the leading enemy
+		var targetPosition = target.global_position # identify where it is
+		look_at(targetPosition) # look at it. LOOK AT IT!
+		# now the projectiles
+		var projectileInstance = projectileScene.instance()
+		projectileInstance.position = position
+		projectileInstance.rotation_degrees = rotation_degrees
+		projectileInstance.apply_impulse(Vector2(), Vector2(projectileInstance.speed, 0).rotated(rotation))
+		get_parent().add_child(projectileInstance)
+
 
--- GD_Project/Towers/Projectiles/Projectile-001.gd
+++ GD_Project/Towers/Projectiles/Projectile-001.gd
@@ -18,8 +18,8 @@ func _physics_process(delta):
 	# finding the distance between the projectile and the tower... 
 	# I believe to_global(position).x/y is getting the right coords for the projectile,
 	# but I have yet to produce the correct coords for the tower...
-	var distanceFromCenter #= sqrt( pow((to_global(???).x - to_global(position).x),2) + pow((to_global(???).y - to_global(position).y),2) )
-	if distanceFromCenter > 100: # for initial testing, I want to greatly limit
+	var distanceFromCenter = get_parent().global_position.distance_to(global_position)#= sqrt( pow((to_global(???).x - to_global(position).x),2) + pow((to_global(???).y - to_global(position).y),2) )
+	if distanceFromCenter > 50: # for initial testing, I want to greatly limit
 				# the distance of the projectiles...
 		queue_free()
 
