c79a7be948e2aab76c500cf1e7f16f714f96565e
Angadeon <angadeon@gmail.com>
2020-08-21 21:33:59 -0700

    cleaned up more unused files

--- GD_Project/Enemies/Sprite.gd
+++
@@ -1,5 +0,0 @@
-extends Sprite
-
-func _ready():
-	modulate = Color(0.6, 0.6, 0.6) # Tint the sprite
-	
--- GD_Project/Enemies/Sprite2.gd
+++
@@ -1,4 +0,0 @@
-extends Sprite
-
-func _ready():
-	modulate = Color(0.0, 0.1, 0.0) # blue shade
