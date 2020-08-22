024d07eba66e6acf6494a1b9cd2728c670201918
Angadeon <angadeon@gmail.com>
2020-08-22 12:59:26 -0700

    fixed pausing after enemy death glitch

--- GD_Project/Towers/Tower-001.gd
+++ GD_Project/Towers/Tower-001.gd
@@ -5,13 +5,16 @@ onready var attackRange = $AttackRange
 # importing targets from the range collider, and storing in a list
 var enemiesInRange = []
 
-func _physics_process(delta):
-	if attackRange.targetAcquired:
-		enemiesInRange.append(attackRange.targetAcquired)
-		attackRange.targetAcquired = null
-		#print(enemiesInRange)
-	if attackRange.targetLost:
-		enemiesInRange.erase(attackRange.targetLost)
-		attackRange.targetLost = null
-		#print(enemiesInRange)
+# Took these triggers out of the physics process and it fixed the "pause after death" problem
+var targetAcquired = null
+var targetLost = null
+func _on_AttackRange_body_entered(body):
+	#print("Enemy sighted!")
+	enemiesInRange.append(body)
+func _on_AttackRange_body_exited(body):
+	#print("Enemy out of range.")
+	enemiesInRange.erase(body)
 
+
+func _physics_process(delta):
+	pass
